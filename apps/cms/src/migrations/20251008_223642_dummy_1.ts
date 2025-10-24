46dd5e74f6b8c3a8f53d7805afe240d6d4a10373
Leonid Belyaev <lbeliaev@gmail.com>
2025-10-24 11:43:10 -0700

    changed PG driver, updated migrations

--- apps/cms/src/migrations/20251008_223642_dummy_1.ts
+++ apps/cms/src/migrations/20251008_223642_dummy_1.ts
@@ -1,13 +1,13 @@
-import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-vercel-postgres'
+import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
 
 export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   // Migration code
-  await db.execute(sql`
+  await payload.db.drizzle.execute(sql`
    CREATE UNIQUE INDEX "dashboards_owner_user_id_idx" ON "dashboards" USING btree ("owner_user_id");`)
 }
 
 export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
   // Migration code
-  await db.execute(sql`
+  await payload.db.drizzle.execute(sql`
    DROP INDEX "dashboards_owner_user_id_idx";`)
 }
--- apps/cms/src/migrations/20251010_202527_dashboard_item_add_height.ts
+++ apps/cms/src/migrations/20251010_202527_dashboard_item_add_height.ts
@@ -1,11 +1,11 @@
-import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-vercel-postgres'
+import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
 
 export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
-  await db.execute(sql`
+  await payload.db.drizzle.execute(sql`
    ALTER TABLE "dashboard_items" ADD COLUMN "height" numeric DEFAULT 4;`)
 }
 
 export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
-  await db.execute(sql`
+  await payload.db.drizzle.execute(sql`
    ALTER TABLE "dashboard_items" DROP COLUMN "height";`)
 }
