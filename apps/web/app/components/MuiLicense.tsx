0cdc36bbb4751a6f2cc3cd98c7fc863f2b049168
Leonid Belyaev <lbeliaev@gmail.com>
2025-11-17 14:56:48 -0800

    mui-x license key debug

--- apps/web/app/components/MuiLicense.tsx
+++ apps/web/app/components/MuiLicense.tsx
@@ -4,7 +4,9 @@ import { LicenseInfo } from "@mui/x-license";
 
 LicenseInfo.setLicenseKey(process.env.NEXT_PUBLIC_MUIX_LICENSE_KEY!);
 
-const MuiXLicense = () =>
-  // console.log("MUI Key:", process.env.NEXT_PUBLIC_MUIX_LICENSE_KEY?.length);
-  null;
+const MuiXLicense = () => {
+  console.log("MUI Key:", process.env.NEXT_PUBLIC_MUIX_LICENSE_KEY?.length);
+  return null;
+};
+
 export default MuiXLicense;
